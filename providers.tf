91d1ccc1f1a717185c928d854c431ee6f50ec41b
Nick Narcise <nick@alternative.co>
2023-12-13 19:22:33 -0500

    Add backend to providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "n2solutions-io"
+
+    workspaces {
+      name = "k8s-gcp-iac"
+    }
+  }
+}
 provider "google" {
     project = "k8s-tfc"
     region = "us-east1"
