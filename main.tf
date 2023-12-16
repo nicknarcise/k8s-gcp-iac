b059ef666b91d01a250a72615012c36c5ce63531
Nick Narcise <nick@alternative.co>
2023-12-16 12:44:20 -0500

    Add controller and worker nodes

--- main.tf
+++ main.tf
@@ -47,3 +47,59 @@ resource "google_compute_address" "vpc_address" {
   name = "kubernetes-the-hard-way"
   region = "us-east1"
 }
+resource "google_compute_instance" "controller" {
+  count = 3
+  name = "controller-${count.index}"
+  machine_type = "e2-standard-2"
+  zone = "us-east1-b"
+  tags = ["kubernetes-the-hard-way", "controller"]
+  can_ip_forward = true
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20200414"
+      size = 200
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.id
+    network_ip = "10.240.0.1${count.index}"
+    subnetwork = google_compute_subnetwork.vpc_subnetwork.id
+    access_config {
+      nat_ip = google_compute_address.vpc_address.address
+    }
+  }
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
+}
+
+# Worker Nodes
+resource "google_compute_instance" "worker" {
+  count = 3
+  name = "worker-${count.index}"
+  machine_type = "e2-standard-2"
+  zone = "us-east1-b"
+  tags = ["kubernetes-the-hard-way", "worker"]
+  can_ip_forward = true
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20200414"
+      size = 200
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.id
+    network_ip = "10.240.0.2${count.index}"
+    subnetwork = google_compute_subnetwork.vpc_subnetwork.id
+    access_config {
+      nat_ip = google_compute_address.vpc_address.address
+    }
+  }
+
+  metadata = {
+    pod-cidr = "10.200.${count.index}.0/24"
+  }
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
+}
