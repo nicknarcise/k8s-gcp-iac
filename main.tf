443d8cdcf252881ccac373246ce5a052a62b7880
Nick Narcise <nick@alternative.co>
2023-12-16 11:37:32 -0500

    Add subnet and internal and external firewall rules

--- main.tf
+++ main.tf
@@ -2,3 +2,43 @@ resource "google_compute_network" "vpc_network" {
   name = "kubernetes-the-hard-way"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name = "kubernetes"
+  ip_cidr_range = "10.240.0.0/24"
+  region = "us-east1"
+  network = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "vpc_firewall" {
+  name = "kubernetes-the-hard-way-allow-internal"
+  network = google_compute_network.vpc_network.id
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    #ports = ["22", "6443"]
+  }
+  allow {
+    protocol = "udp"
+  }
+  source_ranges = ["10.240.0.0/24","10.200.0.0/16"]
+}
+
+resource "google_compute_firewall" "vpc_firewall_external" {
+  name = "kubernetes-the-hard-way-allow-external"
+  network = google_compute_network.vpc_network.id
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports = ["22", "6443"]
+  }
+  #allow {
+  #  protocol = "udp"
+  #  ports = ["53"]
+  #}
+  source_ranges = ["0.0.0.0/0"]
+}
